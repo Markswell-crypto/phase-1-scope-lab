956fe388f808fa8940dd7f78b41d0c6848e3921d
Markswell <osoromarkswell80@gmail.com>
2023-09-20 19:37:00 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+//var customerName = "bob" ;
+//function upperCaseCustomerName(){
+  //  console.log(customerName.toUpperCase());
+   // return customerName.toUpperCase(customerName);
+
+//}
+
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+const leastFavoriteCustomer = 'name';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'name';
+}
